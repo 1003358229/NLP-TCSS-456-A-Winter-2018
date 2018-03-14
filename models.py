408a0be859848babb7c14cdf5517955b7e081783
1003358229 <dongsheng1han@gmail.com>
2018-03-13 22:31:49 -0700

    Topic for each year

--- models.py
+++ models.py
@@ -18,177 +18,177 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 
 
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2017/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2017/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2017/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-####print(list(corpus))
-####print(dictionary)
-####print(dictionary.token2id)
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-####for doc in corpus_tfidf:
-####    print(doc)
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('2017----------------------------------------------------------------------------------------------------------------')
-##
-##
-##
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2012/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2012/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2012/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('2012----------------------------------------------------------------------------------------------------------------')
-##
-##
-##
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2007/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2007/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2007/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('2007----------------------------------------------------------------------------------------------------------------')
-##
-##
-##
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2002/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2002/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2002/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##    
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('2002----------------------------------------------------------------------------------------------------------------')
-##
-##
-##
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1997/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1997/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1997/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('1997----------------------------------------------------------------------------------------------------------------')
-##
-##
-##
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1992/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1992/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1992/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('1992----------------------------------------------------------------------------------------------------------------')
-##
-##
-##
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1987/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1987/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1987/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('1987----------------------------------------------------------------------------------------------------------------')
-##
-##
-##
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1982/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1982/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1982/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('1982----------------------------------------------------------------------------------------------------------------')
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2017/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2017/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2017/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+##print(list(corpus))
+##print(dictionary)
+##print(dictionary.token2id)
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+##for doc in corpus_tfidf:
+##    print(doc)
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('2017----------------------------------------------------------------------------------------------------------------')
+
+
+
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2012/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2012/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2012/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('2012----------------------------------------------------------------------------------------------------------------')
+
+
+
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2007/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2007/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2007/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('2007----------------------------------------------------------------------------------------------------------------')
+
+
+
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2002/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2002/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/2002/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+    
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('2002----------------------------------------------------------------------------------------------------------------')
+
+
+
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1997/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1997/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1997/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('1997----------------------------------------------------------------------------------------------------------------')
+
+
+
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1992/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1992/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1992/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('1992----------------------------------------------------------------------------------------------------------------')
+
+
+
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1987/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1987/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1987/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('1987----------------------------------------------------------------------------------------------------------------')
+
+
+
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1982/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1982/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1982/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('1982----------------------------------------------------------------------------------------------------------------')
 
 
 
@@ -207,30 +207,30 @@ print('10-----------------------------------------------------------------------
 
 '''LSI topics'''
 '''need to be more than 2(3 or more)'''
-lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=3)
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
 ##lsi.print_topics(100)
 print('1977----------------------------------------------------------------------------------------------------------------')
 
 
 
-##'''read generated files'''
-##if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1972/tmp/dictionary.dic")):
-##    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1972/tmp/dictionary.dic')
-##    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1972/tmp/corpus.cop')
-##    print("Used files generated")
-##else:
-##    print("Please generate data set")
-##
-##'''TF-IDF'''
-##tfidf = models.TfidfModel(corpus)
-##corpus_tfidf = tfidf[corpus]
-##print('10----------------------------------------------------------------------------------------------------------------')
-##
-##'''LSI topics'''
-##'''need to be more than 2(3 or more)'''
-##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
-####lsi.print_topics(100)
-##print('1972----------------------------------------------------------------------------------------------------------------')
+'''read generated files'''
+if (os.path.exists("D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1972/tmp/dictionary.dic")):
+    dictionary = corpora.Dictionary.load('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1972/tmp/dictionary.dic')
+    corpus = corpora.MmCorpus('D:/HOME/TCSS456/NLP-TCSS-456-A-Winter-2018/1972/tmp/corpus.cop')
+    print("Used files generated")
+else:
+    print("Please generate data set")
+
+'''TF-IDF'''
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+print('10----------------------------------------------------------------------------------------------------------------')
+
+'''LSI topics'''
+'''need to be more than 2(3 or more)'''
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=1)
+##lsi.print_topics(100)
+print('1972----------------------------------------------------------------------------------------------------------------')
 
 
 
